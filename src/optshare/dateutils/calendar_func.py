c9320afa1820caf220b2471926d7885fc002fb2f
luyu <luyudso@gmail.com>
2023-02-03 15:29:13 +0800

    finalize date utility func

--- src/optshare/dateutils/calendar_func.py
+++ src/optshare/dateutils/calendar_func.py
@@ -2,8 +2,18 @@ import os, requests
 from datetime import datetime, timedelta
 import warnings
 
+
 class Calendar:
+    """ This module provides the interface for determining whether a date is business day or holiday via specifying a txt file with holiday list.
+
+    """
+
     def __init__(self, filename):
+        """ Specify holiday list with a txt file
+
+        :param filename: a txt file with holiday list, use remote url instead if cannot find file
+        :type filename: str
+        """
         self.filename = filename
         self.data_path = os.path.join(os.path.dirname(__file__), filename)
         try:
@@ -11,51 +21,102 @@ class Calendar:
                 holiday_dates = f.readlines()
 
         except FileNotFoundError:
-            warnings.warn("Cannot recognize filename, use remote holiday list instead")
+            warnings.warn("Cannot recognize filename, use remote Chinese holiday list instead")
             response = requests.get(
-         'https://raw.githubusercontent.com/yulu0131/optshare/master/src/optshare/date/China.txt')
+                'https://raw.githubusercontent.com/yulu0131/optshare/master/src/optshare/date/China.txt')
             holiday_dates = list(filter(None, response.text.split('\n')))
 
-        self._holiday_rule = [datetime.strptime(holiday_date.rstrip('\n'), "%Y-%m-%d").date() for holiday_date in holiday_dates]
+        self._holiday_rule = [datetime.strptime(holiday_date.rstrip('\n'), "%Y-%m-%d").date() for holiday_date in
+                              holiday_dates]
 
-    def is_business_day(self, input_date):
-        return input_date.weekday()<5 and input_date not in self._holiday_rule
+    def is_business_day(self, d):
+        """ A boolean value that determines whether d is business day
 
-    def is_holiday(self, input_date):
-        return not self.is_business_day(input_date)
+        :param d: input date
+        :type d: datetime.date
+        :return: True or False. True if d is business day, and vice versa.
+        :rtype: bool
+        """
+        return d.weekday() < 5 and d not in self._holiday_rule
 
-    def next_business_day(self, input_date):
+    def is_holiday(self, d):
+        """ A boolean value that determines whether d is holiday
+
+        :param d: input date
+        :type d: datetime.date
+        :return: True or False. True if d is holiday, and vice versa.
+        :rtype: bool
+        """
+        return not self.is_business_day(d)
+
+    def next_business_day(self, d):
+        """ Returns the following business day given input date d
+
+        :param d: input date
+        :type d: datetime.date
+        :return: next business day
+        :rtype: datetime.date
+        """
         while True:
-            input_date = input_date + timedelta(days=1)
-            if self.is_business_day(input_date):
+            d = d + timedelta(days=1)
+            if self.is_business_day(d):
                 break
-        return input_date
+        return d
 
-    def previous_business_day(self, input_date):
+    def previous_business_day(self, d):
+        """ Returns previous business day given input date d
+
+        :param d: input date
+        :type d: datetime.date
+        :return: previous business day
+        :type: datetime.date
+        """
         while True:
-            input_date = input_date - timedelta(days=1)
-            if self.is_business_day(input_date):
+            d = d - timedelta(days=1)
+            if self.is_business_day(d):
                 break
-        return input_date
+        return d
 
-    def business_days_between(self, start_date, end_date):
-        #todo: test remains
+    def business_days_between(self, start_date, end_date, include_start_date=False, include_end_date=True):
+        """ Count the number of business days between given start date and end date, specifying whether to include start date or end date
+
+        :param start_date: start date
+        :type start_date: datetime.date
+        :param end_date: end date
+        :type end_date: datetime.date
+        :param include_start_date: determine whether to include start date
+        :type include_start_date: bool
+        :param include_end_date: determine whether to include end date
+        :type include_end_date: bool
+        :return: the number of business days between start date and end date
+        :rtype: bool
+        """
+
         if start_date > end_date:
             raise Exception("Start date is greater than end date!")
+
         current_date = start_date
+        days = 0
         while current_date <= end_date:
             if self.is_business_day(current_date):
-                yield current_date
-            current_date = current_date + timedelta(days = 1)
-        return current_date
+                days += 1
+            current_date = current_date + timedelta(days=1)
+
+        if not include_start_date:
+            days -= 1
+        if not include_end_date:
+            days -= 1
+        return days
 
 
 if __name__ == '__main__':
     # test datafile
     cal = Calendar("China.txt")
     from datetime import date
-    test_date = date(2023,10,1)
-    justify_business_day = cal.is_business_day(test_date)
-    justify_holiday = cal.is_holiday(test_date)
-    next_businessday = cal.next_business_day(test_date)
-    print(next_businessday)
+
+    date1 = date(2023, 10, 1)
+    date2 = date(2023, 11, 1)
+    # justify_business_day = cal.is_business_day(test_date)
+    # justify_holiday = cal.is_holiday(test_date)
+    days_between = cal.business_days_between(date1, date2)
+    print("optshare results is ", days_between)
--- src/optshare/dateutils/date_utility.py
+++ src/optshare/dateutils/date_utility.py
@@ -1,5 +1,18 @@
 import datetime
+from datetime import date, datetime, timedelta
+
+""" This module provides you with useful date utility functions in the Chinese financial market for more convenient research purpose.
+
+"""
+
 def get_market_time(now):
+    """ Adjusted current datetime function.
+
+    :param now: current datetime, e.g. datetime.datetime.now()
+    :type now: datetime.datetime
+    :return: corresponding market time, adjusting the dates that are not in the transaction
+    :rtype: datetime.datetime
+    """
     market_morning_open = now.replace(hour=9, minute=30, second=0, microsecond=0)
     market_morning_close = now.replace(hour=11, minute=30, second=0, microsecond=0)
     market_afternoon_open = now.replace(hour=13, minute=0, second=0, microsecond=0)
@@ -17,14 +30,6 @@ def get_market_time(now):
         return market_afternoon_close
 
 
-def date_from_string(date_str):
-    """
-    :param date_str: dateutils string, e.g. "2022-01-01"
-    :return: datetime.dateutils
-    """
-    return datetime.date(int(date_str[:4]), int(date_str[5:7]), int(date_str[8:10]))
-
-
 def get_weekday_in_month(year, month, number_of_week, weekday):
     """ get the nth weekday in month
 
@@ -39,6 +44,7 @@ def get_weekday_in_month(year, month, number_of_week, weekday):
     :return: nth weekday in month
     :rtype: datetime.date
     """
+
     temp = datetime.date(year, month, 1)
     adj = (weekday - temp.weekday()) % 7 - 1
     temp += datetime.timedelta(days=adj)
@@ -46,43 +52,72 @@ def get_weekday_in_month(year, month, number_of_week, weekday):
     return temp
 
 
+def date_from_string(date_str):
+    """ Get datetime.date object from 'yyyymmdd' date string
 
-# def count_days(start_date_str, end_date_str, calendar_type, include_start_date=False, include_end_date=True):
-#     start_date = date_from_string(start_date_str)
-#     end_date = date_from_string(end_date_str)
-#     days_to_add = 0
-#     if include_start_date:
-#         days_to_add += 1
-#     if include_end_date:
-#         days_to_add += 1
-#     if calendar_type == '自然日':
-#         return end_date - start_date - 1 + days_to_add
-#     if calendar_type == '交易日':
-#         return calendar.business_days_between(start_date, end_date, include_start_date, include_end_date)
-#
-#
-# def get_lastnth_trading_day(n, yyyy, mm, cal):
-#     next_month_first_date = opt.Date(yyyy, mm, 1) + opt.Months(1)
-#     d = next_month_first_date - opt.Days(1)
-#     i = 0
-#     if cal.is_businessday(d):
-#         i += 1
-#     while i < n:
-#         d = cal.previous_businessday(d)
-#         i += 1
-#     return d
-#
-#
-# def get_nth_trading_day(n, yyyy, mm, cal):
-#     d = opt.Date(yyyy, mm, 1)
-#     i = 0
-#     if cal.is_businessday(d):
-#         i += 1
-#     while i < n:
-#         d = cal.next_businessday(d)
-#         i += 1
-#     return d
+    :param date_str: date string, e.g. "20220101"
+    :type date_str: str, 'yyyymmdd'
+    :return: date object
+    :rtype: datetime.date
+    """
+    return date(int(date_str[:4]), int(date_str[4:6]), int(date_str[6:]))
+
+
+def count_natural_days(start_date_str, end_date_str, include_start_date=False, include_end_date=True):
+
+    start_date = date_from_string(start_date_str)
+    end_date = date_from_string(end_date_str)
+    days_to_add = 0
+    if include_start_date:
+        days_to_add += 1
+    if include_end_date:
+        days_to_add += 1
+
+    return end_date - start_date - 1 + days_to_add
+
+def count_business_days(start_date_str, end_date_str, cal, include_start_date=False, include_end_date=True):
+    start_date = date_from_string(start_date_str)
+    end_date = date_from_string(end_date_str)
+    return cal.business_days_between(start_date, end_date, include_start_date, include_end_date)
+
+def get_lastnth_trading_day(n, yyyy, mm, cal):
+    def add_months(input_date, months):
+        month = input_date.month - 1 + months
+        year = input_date.year + month // 12
+        month = month % 12 + 1
+        import calendar
+        day = min(input_date.day, calendar.monthrange(year, month)[1])
+        return datetime.date(year, month, day)
+
+    first_date = date(yyyy, mm, 1)
+    next_month_first_date = first_date + add_months(first_date, 1)
+    d = next_month_first_date - timedelta(days=1)
+    i = 0
+    if cal.is_businessday(d):
+        i += 1
+    while i < n:
+        d = cal.previous_businessday(d)
+        i += 1
+    return d
+
+
+def get_nth_trading_day(n, yyyy, mm, cal):
+    d = date(yyyy, mm, 1)
+    i = 0
+    if cal.is_businessday(d):
+        i += 1
+    while i < n:
+        d = cal.next_businessday(d)
+        i += 1
+    return d
+
 
 if __name__ == "__main__":
 
-    print(get_weekday_in_month(2023, 2, 4, 1))
+    from calendar_func import Calendar
+    cal = Calendar("China.txt")
+    date1 = date(2023, 10, 1)
+    date2 = date(2023, 11, 1)
+    trading_days_between = count_business_days("20231001", "20231101", cal)
+
+    print(trading_days_between)
